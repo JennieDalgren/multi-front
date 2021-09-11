dc771fd36cf145ad35ff65de0d38c6b0de6ceb29
Jennie Dalgren <jennie.dalgren@gmail.com>
2021-09-11 23:43:32 +0200

    add hotkey and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useRef, useState } from 'react';
 import { useMousePosition } from './useMousePosition'
 import { useSockets } from './context/socket.context'
 import EVENTS from "./config/events"
-
-
+import { useHotkeys } from 'react-hotkeys-hook';
 
 function App() {
   const {socket, username, setUsername, users} = useSockets()
   const usernameRef = useRef(null) 
   const position = useMousePosition();
+
+  const [chat, setChat] = useState<boolean>(false);
+  useHotkeys('ctrl+k', () => setChat(state => !state))
+  
 
   function handleSetUsername () {
     {/* @ts-expect-error */}
@@ -24,26 +26,29 @@ function App() {
   }
 
   useEffect(() => {
-    console.log('sending new position to backend')
     // Everytime the position change, we want to send that back to the server
     socket.emit(EVENTS.CLIENT.UPDATE_USER_POSITION, {position})
   }, [position])
 
   return (
     <main className="App">
+      
       <div 
         className="cursor" 
         style={{transform: `translate3d(${position.x}px, ${position.y}px, 0)` }}
       >
         <div className="cursor-wrapper">
           <span>{username || 'guest'}</span>
+          {chat && (<input className="writing" autoFocus/>)}
         </div>
       </div>
       
       {!username && (
-        <div>
-          <input autoFocus placeholder="username" ref={usernameRef} />
-          <button onClick={handleSetUsername}>start</button>
+        <div className="intro">
+          <div className="intro-contents">
+            <input className="input" autoFocus placeholder="username" ref={usernameRef} />
+            <button className="button" onClick={handleSetUsername}>Let's start</button>
+          </div>
         </div>
       )}
 
@@ -64,6 +69,8 @@ function App() {
           }
         })   
       )}
+
+      
     </main>
   );
 }
